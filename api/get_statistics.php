1094f8510a7de6ef4b3ebc07ec0c4972cdd08f59
Valentin PINZARU <vpinzaru@bitdefender.com>
2019-06-10 03:44:39 +0300

    endp statistics

---
+++ api/get_statistics.php
@@ -0,0 +1,72 @@
+<?php
+
+include_once 'engine/utils.php';
+include_once 'engine/database.php';
+
+header('Content-Type: application/json');
+
+$data = get_data();
+
+
+if($data['method'] != 'GET')
+{
+    show_result("error",'Wrong request method.',400);
+    exit();
+}
+
+$conn = get_conn();
+
+$statistics = [];
+
+$lost = 0;
+$found = 0;
+
+$sql = "select * from pets";
+
+$result = $conn->query($sql);
+
+$address_ap = [];
+
+while($row = $result->fetch_assoc())
+{
+    $street = explode(",",$row['address'])[0];
+    if(isset($address_ap[$street]))
+    {
+        $address_ap[$street] += 1;
+    }
+    else
+    {
+        $address_ap[$street] = 1;
+    }
+
+    switch($row['status'])
+    {
+        case 1:
+            $found +=1;
+            break;
+        case 2:
+            $lost +=1;
+
+    }
+}
+close_conn($conn);
+
+$max = 0;
+$street = "";
+
+foreach($address_ap as $key => $value)
+{
+    if($value > $max)
+    {
+        $max = $value;
+        $street = $key;
+    }
+}
+
+$statistics['nr_found'] = $found;
+$statistics['nr_lost'] = $lost;
+$statistics['vuln_address'] = $street;
+
+show_result("ok",$statistics,200);
+
+?>
