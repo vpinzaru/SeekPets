05fd78e65dc631ebbcf059746af97f4f2ad498dd
Valentin PINZARU <vpinzaru@bitdefender.com>
2019-06-10 04:24:25 +0300

    get bbox endpoint

---
+++ api/get_bbox.php
@@ -0,0 +1,35 @@
+<?php
+
+include_once 'engine/map.php';
+
+header('Content-Type: application/json');
+
+$data = get_data();
+
+$params = array('latitude', 'longitude');
+
+if ($data['method'] != 'POST') {
+    show_result("error", 'Wrong request method.', 400);
+    exit();
+}
+
+$info = get_payload($data['body'], $params);
+if ($login == 'error') {
+    close_conn($conn);
+    exit();
+}
+
+$box = get_box($info['latitude'], $info['longitude']);
+
+$response = [];
+$response['primul'] = $box['minlon'];
+$response['aldoilea'] = $box['minlat'];
+$response['altreilea'] = $box['maxlon'];
+$response['alpatrulea'] = $box['maxlat'];
+
+show_result("ok",$response,200);
+
+
+
+
+?>
